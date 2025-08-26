61a00f28c810cc9507e94e511d1c2b9f0d269732
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-08-26 12:06:44 +0900

    init

---
+++ src/main/java/com/example/springgsm/SpringgsmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgsmApplication.class, args);
+	}
+
+}
