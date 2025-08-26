e4ad5de4222d06d0bbc47e1c23413d821f471168
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-08-26 12:27:09 +0900

    hello 메소드 추가

---
+++ src/main/java/com/example/springgsm/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springgsm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "나는 이상혁이야";
+    }
+}
